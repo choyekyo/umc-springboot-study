869d94ee78ca9f6b0bab540aab8f226b72a43d80
choyekyo <qkdxksxoxo@naver.com>
2026-03-03 17:44:20 +0900

    project setting

---
+++ src/main/java/com/example/umcspringbootstudy/UmcSpringbootStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.umcspringbootstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmcSpringbootStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UmcSpringbootStudyApplication.class, args);
+    }
+
+}
